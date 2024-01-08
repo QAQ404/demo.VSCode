734f030d501e33ed29954e4e48e058747e2fa8f4
2020211001001115HeZhihao <1928811376@qq.com>
2024-01-08 20:19:18 +0800

    书接上回，CNM！！！

--- src/views/user/UserResetPassword.vue
+++ src/views/user/UserResetPassword.vue
@@ -1,60 +1,70 @@
 <script setup>
 import { ref } from 'vue'
 
-const passwordData = ref({
-    old_pwd: '',
-    new_pwd: '',
-    re_pwd: ''
+const registerData2 = ref({
+    password2: "",
+/*     new_pwd: "",
+    re_pwd: "" */
 })
-
+const registerData = ref({
+    password: "",
+    // rePassword: ""
+})
 //校验密码的函数
-const checkRePassword = (rule, value, callback) => {
+/* const checkRePassword = (rule, value, callback) => {
     if (value == '') callback(new Error('请再次输入密码'))
     if (value !== registerData.value.password) callback(new Error("两次密码输入不一致"))
     else callback()
-}
+} */
 //定义表单校验规则
 const rules = {
-    username: [
+   /*  username: [
         { required: true, message: "请输入账号", trigger: "blur" },
         { min: 5, max: 16, message: "长度必须为5-16位", trigger: "change" },
         { min: 5, max: 16, message: "长度必须为5-16位", trigger: "blur" }
-    ],
+    ], */
     password: [
         { required: true, message: "请输入密码", trigger: "blur" },
-        { pattern:"^\\S{5,16}$", message: "长度必须为5-16位", trigger: "change" },
-        { pattern:"^\\S{5,16}$", message: "长度必须为5-16位", trigger: "blur" },
+        { pattern: "^\\S{5,16}$", message: "长度必须为5-16位", trigger: "change" },
+        { pattern: "^\\S{5,16}$", message: "长度必须为5-16位", trigger: "blur" },
     ],
-    rePassword: [
+    /* rePassword: [
         // { required: true, message: "必须输入", trigger: "blur" },
         { validator: checkRePassword, trigger: "blur" }
-    ]
+    ] */
 }
 
-const changePassword = () => {
+/* const changePassword = () => {
     console.log(passwordData.value.new_pwd);
-}
+} */
 
 </script>
 <template>
-    
-        <el-row>
-            <el-col :span="12">
-                <el-form :model="passwordData" :rules="rules" label-width="100px" size="large">
-                    <el-form-item label="现有密码" prop="password">
-                        <el-input  v-model="passwordData.old_pwd"></el-input>
-                    </el-form-item>
-                    <el-form-item label="新密码" prop="password">
-                        <el-input v-model="passwordData.new_pwd"></el-input>
-                    </el-form-item>
-                    <el-form-item label="请再次输入" prop="password">
-                        <el-input v-model="passwordData.re_pwd"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="changePassword">修改密码</el-button>
-                    </el-form-item>
-                </el-form>
-            </el-col>
-        </el-row>
-    
+    <el-row>
+        <el-col :span="12">
+            <el-form :model="registerData2" :rules="rules">
+                <el-form-item  prop="password">
+                    <el-input v-model="registerData2.password2"></el-input>
+                </el-form-item>
+                <!-- <el-form-item  prop="password">
+                    <el-input v-model="passwordData.new_pwd"></el-input>
+                </el-form-item> -->
+                <!-- <el-form-item label="请再次输入" prop="password">
+                    <el-input v-model="passwordData.re_pwd"></el-input>
+                </el-form-item> -->
+                <!-- <el-form-item>
+                    <el-button type="primary" @click="changePassword">修改密码</el-button>
+                </el-form-item> -->
+            </el-form>
+            -------------------------------------------------------------------------
+            <el-form :model="registerData" :rules="rules">
+                <el-form-item prop="password">
+                    <el-input  v-model="registerData.password"></el-input>
+                </el-form-item>
+                <!-- <el-form-item prop="rePassword">
+                    <el-input  v-model="registerData.rePassword"></el-input>
+                </el-form-item> -->
+            </el-form>
+        </el-col>
+    </el-row>
 </template>
